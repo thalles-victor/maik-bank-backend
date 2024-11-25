267c74f208869f73309c5cd5b37bcd23215f0e28
thalles-victor <thalles.victor2012@gmail.com>
2024-11-25 16:50:34 -0300

    :wrench: conf: sequilize module with postgres.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { env } from '@utils';
+import { UserModel } from '#models';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: env.POSTGRES_HOST,
+      port: env.POSTGRES_PORT,
+      username: env.POSTGRES_USER,
+      password: env.POSTGRES_PASSWORD,
+      database: env.POSTGRES_DB,
+      models: [UserModel],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
