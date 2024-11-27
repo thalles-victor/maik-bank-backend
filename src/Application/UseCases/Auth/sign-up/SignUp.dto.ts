773cd8ea67570c5616de5ebe4691d52549437cdb
thalles-victor <thalles.victor2012@gmail.com>
2024-11-27 10:38:25 -0300

    :bug: fix: validators and enable class validator.

--- src/Application/UseCases/Auth/sign-up/SignUp.dto.ts
+++ src/Application/UseCases/Auth/sign-up/SignUp.dto.ts
@@ -2,29 +2,25 @@ import {
   IsEmail,
   IsNotEmpty,
   IsString,
+  Length,
   Matches,
-  Max,
-  Min,
 } from 'class-validator';
 
 export class AuthSignUpDto {
   @IsString()
   @IsNotEmpty()
-  @Min(5)
-  @Max(70)
+  @Length(1, 70)
   name: string;
 
   @IsString()
   @IsNotEmpty()
   @IsEmail()
-  @Min(5)
-  @Max(70)
+  @Length(1, 70)
   email: string;
 
   @IsString()
   @IsNotEmpty()
-  @Min(6)
-  @Max(30)
+  @Length(1, 20)
   @Matches(/^(?=.*[A-Z])(?=.*[!#@$%&])(?=.*[0-9])(?=.*[a-z]).{8,100}$/, {
     message:
       'A senha deve ter 8 carecteres, um especial um maúsculo e um número no mínimo',
@@ -33,6 +29,7 @@ export class AuthSignUpDto {
 
   @IsString()
   @IsNotEmpty()
+  @Length(1, 40)
   @Matches(
     /^([0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}|[0-9]{2}\.?[0-9]{3}\.?[0-9]{3}\/?[0-9]{4}\-?[0-9]{2})$/,
     {
@@ -44,7 +41,8 @@ export class AuthSignUpDto {
 
   @IsString()
   @IsNotEmpty()
-  @Matches(/^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/(19|20)\d{2}$/, {
+  @Length(1, 10)
+  @Matches(/^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/, {
     message:
       'A data de nascimento deve estar no formato da data deve ser dd/mm/aaaa',
   })
--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import { env } from 'src/@shared/@utils';
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   app.enableVersioning();
   await app.listen(env.BACKEND_PORT ?? 3000);
 }
