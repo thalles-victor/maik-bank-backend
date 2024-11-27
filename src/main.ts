be92a67b5f8011c8a19111a30992ace8eed9abe3
thalles-victor <thalles.victor2012@gmail.com>
2024-11-27 08:29:09 -0300

    :wrench: conf: enable global api version.

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableVersioning();
   await app.listen(env.BACKEND_PORT ?? 3000);
 }
 bootstrap();
