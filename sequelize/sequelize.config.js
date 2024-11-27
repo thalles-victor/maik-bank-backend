00ddb7c362fc542995b6bf015ff8b237bfcaed89
thalles-victor <thalles.victor2012@gmail.com>
2024-11-27 09:58:43 -0300

    :wrench: conf: sequelize cli.

---
+++ sequelize/sequelize.config.js
@@ -0,0 +1,25 @@
+import 'dotenv/config';
+
+export default {
+  development: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOST,
+    dialect: 'postgres',
+  },
+  // "test": {
+  //   "username": "root",
+  //   "password": null,
+  //   "database": "database_test",
+  //   "host": "127.0.0.1",
+  //   "dialect": "mysql"
+  // },
+  // "production": {
+  //   "username": "root",
+  //   "password": null,
+  //   "database": "database_production",
+  //   "host": "127.0.0.1",
+  //   "dialect": "mysql"
+  // }
+};
