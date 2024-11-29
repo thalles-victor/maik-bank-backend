63f4faee0da4572a152e546fc05bcc0c258ddea0
thalles-victor <thalles.victor2012@gmail.com>
2024-11-29 15:12:29 -0300

    :bug: fix: input dto and graphql fields.

--- src/@shared/@Pagination/index.ts
+++ src/@shared/@Pagination/index.ts
@@ -1,4 +1,4 @@
-import { ObjectType } from '@nestjs/graphql';
+import { Field, InputType } from '@nestjs/graphql';
 import {
   IsObject,
   IsOptional,
@@ -10,28 +10,34 @@ import {
 import { PaginationProps } from '@types';
 import { Transform } from 'class-transformer';
 
-@ObjectType()
+@InputType()
 export class PaginationDto implements PaginationProps {
+  @Field(() => Number, { defaultValue: 1 })
   @IsOptional()
   @IsInt()
   @Min(1, { message: 'The page must be at least 1' })
   @Transform(({ value }) => parseInt(value, 10), { toClassOnly: true })
   page?: number = 1;
 
+  @Field(() => Number, { defaultValue: 100 })
   @IsOptional()
   @IsInt()
   @Min(1, { message: 'The limit must be at least 1.' })
   @Transform(({ value }) => parseInt(value, 10), { toClassOnly: true })
+  @Field(() => Number)
   limit?: number = 100;
 
+  @Field(() => String, { nullable: true })
   @IsOptional()
   @IsString()
   search?: string;
 
+  @Field(() => String, { nullable: true })
   @IsOptional()
   @IsObject()
   filters?: Record<string, any>;
 
+  @Field(() => String, { defaultValue: 'DESC' })
   @IsString()
   @IsOptional()
   @IsIn(['ASC', 'DESC'], { message: 'Order must be ASC or DESC' })
